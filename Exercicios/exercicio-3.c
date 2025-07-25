576c366168e8fdecb396666ae0bb7af8a122b87d
gb1456v <gabrielvieira794@gmail.com>
2025-07-25 17:53:13 -0300

    Adiciona exercício em C para calcular o tempo jogado em horas e minutos

---
+++ Exercicios/exercicio-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int tempo_jogado,horas, minutos;
+
+    printf("Digite o tempo jogado em minutos\n");
+    scanf("%d",tempo_jogado);
+
+    horas = tempo_jogado / 60;
+    minutos = tempo_jogado % 60;
+
+    printf("O jogo durou %d horas e %d minutos.", horas, minutos);
+    
+    return 0;
+}
